25ed878ed6161dfe0b5546c1c8790d32053c36a2
Saksham <2020UCP1801@mnit.ac.in>
2021-08-17 17:28:05 +0530

    href & icon bug fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="">
+        <a className="navbar-brand" href="/">
           {props.title}
         </a>
         <button
