ce20fecbb4279268454bcd959647bf9e139538d9
Saksham <2020UCP1801@mnit.ac.in>
2021-08-17 17:26:39 +0530

    text state made & upper case functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form() {
+  // Declaring textarea value state
+  const [text, setText] = useState("");
+
+  // Handling onChnage textarea event
+  const handleChangeText = (e) => setText(e.target.value);
+
+  // Function to change into uppercase
+  const handleUpCase = () => setText(text.toUpperCase());
+
   return (
     <div className="container my-3">
       <div className="mb-3">
         <label htmlFor="myText" className="form-label">
           Enter Text here
         </label>
-        <textarea className="form-control" id="myText" rows="10"></textarea>
+        <textarea
+          value={text}
+          onChange={handleChangeText}
+          className="form-control"
+          id="myText"
+          rows="10"
+          placeholder="Enter here"
+        ></textarea>
       </div>
-      <button className="btn btn-primary">Upper Case</button>
+      <button onClick={handleUpCase} className="btn btn-primary">
+        Upper Case
+      </button>
     </div>
   );
 }
