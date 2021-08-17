7321092925c89ab12df60d6cd96bfe285dbcee7b
Saksham <2020UCP1801@mnit.ac.in>
2021-08-17 17:15:39 +0530

    navbar component created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar title="TextUtils" />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Navbar(props) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="">
+          {props.title}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+// Describing types of props passed from app.js
+Navbar.propTypes = { title: PropTypes.string.isRequired };
+
+// Defining default props if not given
+Navbar.defaultProps = { title: "Title Here" };
